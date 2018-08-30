3a02faf71f6c0a94be4adb4d8d529b137ea6cc1d
Leah Joyce <leahpjoyce@gmail.com>
2018-08-30 15:44:38 -0500

    test: 2 ways Declaring Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React, { Component } from 'react';
+/* 2 Ways sample Declaring Components in React
+Source: Rendering UI with React 2. Creating Elements and JSX
+*/
+
+/*import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 
-//React.createElement( /* type */, /* props */, /* content */ );
 
 class ContactList extends Component {
+  render() {
+    const people = [
+      { name: 'Michael' },
+      { name: 'Ryan' },
+      { name: 'Tyler' }
+    ]
+
+    return <ol>
+      {people.map(person =>(
+        <li key={person.name}>{person.name}</li>
+      ))}
+      </ol>
+
+  }
+}
+*/
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class ContactList extends React.Component {
   render() {
     const people = [
       { name: 'Michael' },
