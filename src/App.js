acbd1fc47a9255da53882ecd9de44de5b416de69
Leah Joyce <leahpjoyce@gmail.com>
2018-08-30 14:31:33 -0500

    feat: Add .createElement method w/ className

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = React.createElement('div', null, 'hello world')
+const element = React.createElement('div',
+  { className: 'welcome-message'
+  }, 'hello world')
 
 ReactDOM.render(element, document.getElementById('root'))
