63ab6670a4c0e5e700d110de1025dc96fd108185
Leah Joyce <leahpjoyce@gmail.com>
2018-08-30 14:39:21 -0500

    feat: Add .createElement method content li

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = React.createElement('div',
-  { className: 'welcome-message'
-  }, 'hello world')
+//React.createElement( /* type */, /* props */, /* content */ );
+const element = React.createElement('ol', null,
+  React.createElement('li', null, 'Michael'),
+  React.createElement('li', null, 'Ryan'),
+  React.createElement('li', null, 'Tyler')
+);
 
 ReactDOM.render(element, document.getElementById('root'))
