491140924cb07084e504765c05e2389f5600ce92
Leah Joyce <leahpjoyce@gmail.com>
2018-08-30 16:47:40 -0500

    feat: Render App Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-/*
-ReactDOM.render(<element />, document.getElementById('root'));
- registerServiceWorker();
-*/
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+registerServiceWorker();
