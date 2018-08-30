02600149b5a7879026264aaa3a9b9848d82b0600
Leah Joyce <leahpjoyce@gmail.com>
2018-08-30 14:43:24 -0500

    feat: Add .createElement method w/ .map

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 //React.createElement( /* type */, /* props */, /* content */ );
+
+const people = [
+  { name: 'Michael' },
+  { name: 'Ryan' },
+  { name: 'Tyler' }
+]
+
 const element = React.createElement('ol', null,
-  React.createElement('li', null, 'Michael'),
-  React.createElement('li', null, 'Ryan'),
-  React.createElement('li', null, 'Tyler')
-);
+  people.map((person, index) => (
+    React.createElement('li', { key: index }, person.name)
+  ))
+)
 
 ReactDOM.render(element, document.getElementById('root'))
