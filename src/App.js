adcc5272ba91803b2ac31c6774470a0f12642564
Leah Joyce <leahpjoyce@gmail.com>
2018-08-30 15:27:21 -0500

    feat: Add class ContactList Component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom'
 
 //React.createElement( /* type */, /* props */, /* content */ );
 
-const people = [
-  { name: 'Michael' },
-  { name: 'Ryan' },
-  { name: 'Tyler' }
-]
+class ContactList extends React.Component {
+  render() {
+    const people = [
+      { name: 'Michael' },
+      { name: 'Ryan' },
+      { name: 'Tyler' }
+    ]
 
-const element = <ol>
-  {people.map(person =>(
-    <li key={person.name}>{person.name}</li>
-  ))}
-  </ol>
+    return <ol>
+      {people.map(person =>(
+        <li key={person.name}>{person.name}</li>
+      ))}
+      </ol>
 
-//debugger
+  }
+}
 
-ReactDOM.render(element, document.getElementById('root'))
+
+ReactDOM.render(
+  <ContactList/>, document.getElementById('root'))
